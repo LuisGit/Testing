f0d5680a24f2ff9bf00ad8dd64046e380ba35f00
luis alvarado <luis.alvarado@boszdigital>
2014-01-03 15:25:27 -0600

    not allowing empty spaces for first name, last name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
- # validates :email, :f_name, :l_name, :password, :presence => true
+  validates :email, :f_name, :l_name, :password, :presence => true
   validates :email, :uniqueness =>true
   validates :email, :format =>{:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, :message => 'Not a valid email.'}
 end
